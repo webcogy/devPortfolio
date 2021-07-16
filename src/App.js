5f6bdfcaa7e60eeac06f1170521e63fa7577907c
webcogy <ungk456@naver.com>
2021-07-16 14:51:51 +0900

    update : component name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from 'react';
-import DarkModeComponent from './components/DarkModeComponent';
+import DarkMode from './components/DarkMode';
 import FooterContainer from './containers/FooterContainer';
 import Kind1Container from './containers/Kind1Container';
 import Kind2Container from './containers/Kind2Container';
@@ -13,7 +13,7 @@ function App() {
 
     return (
         <div className="App" ref={appRef}> 
-            <DarkModeComponent appRef={appRef} />
+            <DarkMode appRef={appRef} />
             <div className="header">
                 <h1 ref={fetchGoTopTrigger}>김대현 포트폴리오</h1>
             </div>
