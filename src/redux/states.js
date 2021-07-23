05fbce0ffb49c0bb14ebde3700a80dde128a1028
김대현 <ungk456@naver.com>
2021-07-23 17:17:36 +0900

    ADD : java project

--- src/redux/states.js
+++ src/redux/states.js
@@ -3,7 +3,7 @@ const listState = {
     list_kind1: [],  // LIST ALL, WEB, APP, HYBRID
     list_kind2: [],  // LIST ...ETC skill kinds
     kind1: ['ALL', 'WEB', 'APP', 'HYBRID'],
-    kind2: ['react', 'reactNative', 'webview', 'redux', 'contextAPI', 'sass', 'styledComponent', 'api', 'expo', 'websocket'],
+    kind2: ['react', 'reactNative', 'webview', 'redux', 'contextAPI', 'sass', 'styledComponent', 'api', 'expo', 'websocket', 'jsp', 'oracle', 'sql'],
     user_chk_kind2: [],
     inputRef: [],
     listCounting: 5, // 몇개까지 보여줄지 셋팅, more버튼 누를때마다 5개씩 추가
