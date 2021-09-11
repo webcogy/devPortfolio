94067a891b16985fb2ff9e9cafa96bfe77af5b8f
김대현 <ungk456@naver.com>
2021-09-11 12:12:52 +0900

    UPDATE : 다크모드 수정

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,43 +1,53 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import Brightness2Icon from '@material-ui/icons/Brightness2';
 
 function DarkMode({ appRef }) {
     const [isDark, setIsDark] = useState(false);
-    
 
     useEffect(() => {
         const viewMode = localStorage.getItem('viewMode');
-        if( viewMode === 'dark' ) fetchToDark();
+        if( viewMode === 'dark' ) darkMode();
     }, []);
 
-    const fetchToLight = () => {
+    const lightMode = useCallback(() => {
         appRef.current.classList.remove('darkMode') 
         localStorage.removeItem('viewMode');
         setIsDark(false);
-    }
-    
-    const fetchToDark = () => {
+    },[]);
+
+    const darkMode = useCallback(() => {
         appRef.current.classList.add('darkMode') 
         localStorage.setItem('viewMode', 'dark');
         setIsDark(true);
-    }
+    },[]);
+    
+    const fetchToMode = useCallback(() => {
+        if(isDark){
+            lightMode();
+        }else{
+            darkMode();
+        }
+    },[isDark]);
 
 
     return (
-        <div className="darkModeComponent">
+        <button className={
+            isDark === false
+            ? "darkModeComponent light"
+            : "darkModeComponent dark"
+        }
+        onClick={fetchToMode}>
+            <div className="icons">
+                <WbSunnyIcon className={!isDark && "light"} />
+                <Brightness2Icon className={isDark && "dark"} />
+            </div>
             {
                 isDark === false
-                ? <button class="btn_light" onClick={fetchToDark}>
-                    <WbSunnyIcon />
-                    <span>Light</span>
-                </button>
-                : <button class="btn_dark" onClick={fetchToLight}>
-                    <Brightness2Icon />
-                    <span>Dark</span>
-                </button>
+                ? <span>Light</span>
+                : <span>Dark</span>
             }
-        </div>
+        </button>
     );
 }
 
